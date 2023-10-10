6a11e1e68084a665cfe8fbace619cb9d465427d2
Tadas <vtadas25@gmail.com>
2023-10-10 09:39:28 +0300

    Deploy to Goerli to test with Gnosis safe

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -11,10 +11,19 @@ const config: HardhatUserConfig = {
       accounts: [
         process.env.OPGOERLI_PRIV_KEY!,
       ]
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_KEY}`,
+      accounts: [
+        process.env.GOERLI_PRIV_KEY!,
+      ]
     }
   },
   etherscan: {
-    apiKey: process.env.OPGOERLI_ETHERSCAN_KEY
+    apiKey: {
+      optimistiEthereum: process.env.OPGOERLI_ETHERSCAN_KEY!,
+      goerli: process.env.GOERLI_ETHERSCAN_KEY!
+    }
   }
 };
 
